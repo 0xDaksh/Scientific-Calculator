4a20fd8adb01562fe0fb944ea22d9eabc1541d0d
Daksh Miglani <hello@dak.sh>
2017-04-26 22:07:52 +0530

    added welcome function

---
+++ scientifc/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int welcome();
+
+int main() {
+    welcome();
+    return 0;
+}
+
+int welcome() {
+    cout<<"Hello World, Welcome to Scientifc Calculator"<<endl;
+    cout<<"Please, Tell Us What Operation You'd like to use? (trigo, log, add, subtract)"<<endl;
+    return 0;
+}
