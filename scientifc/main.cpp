e3a14041c89a64cae13b94b2be67d5ea973e30a2
Daksh Miglani <hello@dak.sh>
2017-04-26 22:11:50 +0530

    added doTheMath & figure it out

--- scientifc/main.cpp
+++ scientifc/main.cpp
@@ -5,10 +5,14 @@ using namespace std;
 
 int welcome();
 string getOperator();
+string figureOperation(string a);
+int doTheMath(string operation);
 
 int main() {
     welcome();
     string toBeFigured = getOperator();
+    string operation = figureOperation(toBeFigured);
+    doTheMath(operation);
     return 0;
 }
 
@@ -21,4 +25,44 @@ string getOperator() {
     string operation;
     cin>>operation;
     return operation;
+}
+string figureOperation(string a) {
+    string operation = "not defined";
+    if(a == "add" || a == "addition" || a == "sum") {
+        operation = "+";
+    } else if (a == "sub" || a == "subtract" || a == "minus" || a == "difference") {
+        operation = "-";
+    } else if(a == "multiply" || a == "product") {
+        operation = "*";
+    } else if (a == "division" || a == "divide" || a == "div") {
+        operation = "/";
+    } else {
+        operation = "not defined";
+    }
+    return operation;
+}
+int doTheMath(string operation) {
+    float a = 0, b = 0;
+    if(operation == "not defined") {
+        cout<<"Sorry, There's no such Operation defined "<<operation <<endl;
+    } else {
+        cout<<"What's the First Number?"<<endl;
+        cin>>a;
+        cout<<"What's the second Number?"<<endl;
+        cin>>b;
+        if(a == 0 && b == 0) {
+            cout<<"Ouput = 0"<<endl;
+        } else {
+            if(operation == "+") {
+                cout<<"Output = " << a + b << endl;
+            } else if(operation == "-") {
+                cout<<"Output = " << a - b << endl;
+            } else if (operation == "*") {
+                cout<<"Output = " << a * b << endl;
+            } else {
+                cout<<"Output = " << a / b << endl;
+            }
+        }
+    }
+    return 0;
 }
