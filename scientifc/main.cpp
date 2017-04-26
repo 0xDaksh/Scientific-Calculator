6a272bd5c79cbbd24483ab19fd50eb933f4b296c
Daksh Miglani <hello@dak.sh>
2017-04-26 22:09:41 +0530

    added getOperator

--- scientifc/main.cpp
+++ scientifc/main.cpp
@@ -4,9 +4,11 @@
 using namespace std;
 
 int welcome();
+string getOperator();
 
 int main() {
     welcome();
+    string toBeFigured = getOperator();
     return 0;
 }
 
@@ -14,4 +16,9 @@ int welcome() {
     cout<<"Hello World, Welcome to Scientifc Calculator"<<endl;
     cout<<"Please, Tell Us What Operation You'd like to use? (trigo, log, add, subtract)"<<endl;
     return 0;
+}
+string getOperator() {
+    string operation;
+    cin>>operation;
+    return operation;
 }
